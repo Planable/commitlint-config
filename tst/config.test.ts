77f8f47460c9107b8b1a8d2dc7a0f72c84ae0cd3
Dragoș Străinu <dragos@planable.io>
2022-01-20 17:38:31 +0200

    ✅ Add tests for incorrect formats

--- tst/config.test.ts
+++ tst/config.test.ts
@@ -3,38 +3,27 @@ import { rules, parserPreset } from "../src/config";
 
 const lint = (input: string) => commitlint(input, rules, parserPreset);
 
-describe.skip("Invalid commits", () => {
-  // it.each([
-  //   ["No emoji", "Add tests"]
-  // ])("");
-  // it("Add tests -> No emoji", async () => {
-  //   const { valid, errors } = await lint("Add tests");
-  //   expect(valid).toBeFalsy();
-  //   expect(errors).toHaveLength(1);
-  //   const [ error ] = errors;
-  //   expect(error.name).toBe("type-empty");
-  // });
-  it("😂 Add tests -> Wrong emoji", async () => {
-    const o = await lint("😂 Add tests");
-    console.log(JSON.stringify(o, null, 2));
-
-    // expect(valid).toBeFalsy();
-    // expect(errors).toHaveLength(1);
-    // const [ error ] = errors;
-    // expect(error.name).toBe("type-");
-  });
-  it("✅ add tests -> No sentence case", async () => {
-    const { valid, errors } = await lint("✅ add tests");
-    expect(valid).toBeFalsy();
-    expect(errors).toHaveLength(1);
-    const [error] = errors;
-    expect(error.name).toBe("type-empty");
-  });
-  it("✅ [DD] Add tests -> Wrong ticket format", async () => {
-    const { valid, errors } = await lint("✅ [DD] Add tests");
-    expect(valid).toBeFalsy();
-    expect(errors).toHaveLength(1);
-    const [error] = errors;
-    expect(error.name).toBe("type-empty");
-  });
+describe("Invalid commits", () => {
+  test.concurrent.each([
+    ["No emoji", "Add tests", "header-match-planable-pattern"],
+    ["Wrong emoji", "😂 Add tests", "explained-type-enum"],
+    ["No sentence case", "✅ add tests", "subject-case"],
+    ["Wrong ticket format", "✅ [DD] Add tests", "ticket-match-pattern"],
+    ["To much space after emoji", "✅  [P-4605] Add tests", "header-match-planable-pattern"],
+    ["To much space after ticket", "✅ [P-4605]  Add tests", "header-match-planable-pattern"],
+    ["To much space before subject", "✅  Add tests", "header-match-planable-pattern"],
+    ["Not begin with emoji", "_✅ [P-4605] Add tests", "header-match-planable-pattern"],
+    ["Suject starts with non letter", "✅ [P-4605] 5dd tests", "header-match-planable-pattern"],
+    ["Ticket bracket not closed", "✅ [P-4605 Add tests", "header-match-planable-pattern"],
+  ])(
+    "%s: %p should return %p error",
+    // @ts-ignore
+    async (_desc: string, commitMessage: string, expectedError: string) => {
+      const { valid, errors } = await lint(commitMessage);
+      expect(valid).toBeFalsy();
+      expect(errors).toHaveLength(1);
+      const [error] = errors;
+      expect(error.name).toBe(expectedError);
+    }
+  );
 });
