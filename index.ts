df83088b051b39f751d3bd70e2d29bb9f65d6b1d
Pereteatcu Adelina <adelinnafrommars@gmail.com>
2022-01-20 11:17:16 +0200

    👌 Extend Commit type to fit our use case

--- index.ts
+++ index.ts
@@ -5,17 +5,19 @@ import {
   UserConfig,
 } from "@commitlint/types";
 
+declare module "@commitlint/types" {
+  interface Commit {
+    ticket: string | null;
+  }
+}
+
 const anyEmojiWithSpaceAfter =
   /^([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}])\s/u;
 const optionalPlanableTicketWithSpaceAfter = /(?:\[(.+)\]\s)?/;
 const planableTicket = /(P-\d+)/;
 const subjectThatDoesNotStartsWithBracket = /([^[\d\s][a-zA-Z].*)/;
 
-interface ExtendedCommit extends Commit {
-  ticket: string | null;
-}
-
-const headerMatchPlanablePattern = (parsed: ExtendedCommit) => {
+const headerMatchPlanablePattern: Rule = (parsed: Commit) => {
   const { type, ticket, subject } = parsed;
   console.log({ parsed });
   if (type === null && ticket === null && subject === null) {
@@ -27,7 +29,7 @@ const headerMatchPlanablePattern = (parsed: ExtendedCommit) => {
   return [true];
 };
 
-const ticketPattern = (parsed: ExtendedCommit) => {
+const ticketPattern: Rule = (parsed: Commit) => {
   const { ticket } = parsed;
   if (ticket) {
     if (!planableTicket.test(ticket)) {
@@ -78,9 +80,7 @@ const planableConfig: UserConfig = {
   plugins: [
     {
       rules: {
-        // @ts-ignore
         "header-match-planable-pattern": headerMatchPlanablePattern,
-        // @ts-ignore
         "ticket-match-pattern": ticketPattern,
         "explained-type-enum": explainedTypeEnum,
       },
