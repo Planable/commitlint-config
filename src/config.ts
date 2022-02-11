d751da4dc886e47fabe8dc3a1482b239f039f843
Pereteatcu Adelina <adelinnafrommars@gmail.com>
2022-02-11 14:02:08 +0200

    👌 Replace emojis from examples

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,6 @@ declare module "@commitlint/types" {
 const anyEmojiWithSpaceAfter =
   /^([\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}](?:[\u{fe00}-\u{fe0f}])?)\s/u;
 const optionalPlanableTicketWithSpaceAfter = /(?:\[(.+)\]\s)?/;
-const planableTicket = /(^P-\d+$)/;
 const subjectThatDoesNotStartsWithBracket = /([^[\d\s][a-zA-Z].*)/;
 
 const headerMatchPlanablePattern: Rule = (parsed: Commit) => {
@@ -24,13 +23,15 @@ const headerMatchPlanablePattern: Rule = (parsed: Commit) => {
   if (type === null && ticket === null && subject === null) {
     return [
       false,
-      "header must be in format '✅ [P-11] Replace footer' or '✅ Replace footer'",
+      "header must be in format '🌈 [P-11] Replace footer' or '🚧 Replace footer'",
     ];
   }
   return [true];
 };
 
 const ticketPattern: Rule = (parsed: Commit) => {
+  const planableTicket = /(^P-\d+$)/;
+
   const { ticket } = parsed;
   if (ticket) {
     if (!planableTicket.test(ticket)) {
